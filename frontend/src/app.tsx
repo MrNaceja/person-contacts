cc7a150bb2fb1c1f5c835eea0b91524f2b7281f0
Eduardo Toriani <edutoriani13@gmail.com>
2025-06-28 20:42:11 -0300

    build: added react query and setup it

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
-import { Layout } from '@/pages/layout/layout' 
+import { Layout } from '@/pages/layout/layout'
 import { ContactPage } from '@/pages/contact/page'
 import { PersonPage } from '@/pages/person/page'
 import { AuthPage } from '@/pages/auth/page'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" index element={<AuthPage />} />
-        <Route element={<Layout />}>
-          <Route path="/person" element={<PersonPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<AuthPage />} />
+          <Route element={<Layout />}>
+            <Route path="/person" element={<PersonPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
